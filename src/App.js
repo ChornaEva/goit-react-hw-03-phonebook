3feca15909588eaac2f6d84bec55ab86ab7ac546
Chorna Yevheniia <chornaja.evgenija@gmail.com>
2022-01-21 18:20:40 +0200

    saving contacts to local storage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React, { Component } from "react";
 import shortid from "shortid";
-import styled from "styled-components";
+// import styled from "styled-components";
 
 import ContactForm from "./components/ContactForm";
 import Filter from "./components/Filter";
@@ -11,10 +11,10 @@ import { FormContainer, ContactsTitle } from "./App.styled";
 class App extends Component {
   state = {
     contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      // { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
     filter: "",
   };
@@ -45,6 +45,20 @@ class App extends Component {
     const { name, value } = event.currentTarget;
     this.setState({ [name]: value });
   };
+
+  // получаю и записываю в состояние распаршеные контакты из локала
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContatcts = JSON.parse(contacts);
+    this.setState({ contacts: parsedContatcts });
+  }
+
+  // сравнение нового контакта с имеющимся, запись нового в локал
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const normalizedFilter = this.state.filter.toLowerCase();
